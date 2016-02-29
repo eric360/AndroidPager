83f30dfd3af1c7822c44079e3845783685200fd4
eric360 <eric-richard.hongtuanha@360learning.com>
2016-02-29 19:11:03 +0100

    Code Cleant

--- app/src/main/java/eric_hth/my360android/M3LoginActivity.java
+++ app/src/main/java/eric_hth/my360android/M3LoginActivity.java
@@ -1,60 +1,43 @@
 package eric_hth.my360android;
 
+import android.app.ActionBar;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class M3LoginActivity extends AppCompatActivity {
-    TheAdapter adapter;
+    TheAdapter_FragmentStatePager adapter_fragmentStatePager;
     ViewPager pager;
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_m3_login);
-        adapter = new TheAdapter(getSupportFragmentManager());
+        setContentView(R.layout.m3loginactivity);
+        adapter_fragmentStatePager = new TheAdapter_FragmentStatePager(getSupportFragmentManager());
         pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
+        pager.setAdapter(adapter_fragmentStatePager);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(pager);
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     // Since this is an object collection, use a FragmentStatePagerAdapter,
 // and NOT a FragmentPagerAdapter.
-    public class TheAdapter extends FragmentStatePagerAdapter {
-        public TheAdapter(FragmentManager fm) {
+    public class TheAdapter_FragmentStatePager extends FragmentStatePagerAdapter {
+        public TheAdapter_FragmentStatePager(FragmentManager fm) {
             super(fm);
         }
         @Override
         public Fragment getItem(int i) {
-            Fragment fragment = new DemoObjectFragment();
+            Fragment fragment = new TheAdapter_Fragment();
             Bundle args = new Bundle();
-            // Our object is just an integer :-P
-            args.putInt(DemoObjectFragment.ARG_OBJECT, i + 1);
+            args.putInt(TheAdapter_Fragment.ARG_OBJECT, i + 1);
             fragment.setArguments(args);
             return fragment;
         }
@@ -64,13 +47,10 @@ public class M3LoginActivity extends AppCompatActivity {
         }
         @Override
         public CharSequence getPageTitle(int position) {
-            return "OBJECT " + (position + 1);
+            return "Fragment " + (position + 1);
         }
     }
-
-    // Instances of this class are fragments representing a single
-// object in our collection.
-    public static class DemoObjectFragment extends Fragment {
+    public static class TheAdapter_Fragment extends Fragment {
         public static final String ARG_OBJECT = "object";
         @Override
         public View onCreateView(LayoutInflater inflater,
