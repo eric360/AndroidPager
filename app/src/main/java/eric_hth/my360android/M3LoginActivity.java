a0a2956cd32834960846c23bc2d153d1d308bf31
eric360 <eric-richard.hongtuanha@360learning.com>
2016-03-01 20:11:34 +0100

    Code Cleant

--- app/src/main/java/eric_hth/my360android/M3LoginActivity.java
+++ app/src/main/java/eric_hth/my360android/M3LoginActivity.java
@@ -10,39 +10,11 @@ import retrofit.Call;
 import retrofit.Callback;
 import retrofit.Retrofit;
 public class M3LoginActivity extends AppCompatActivity {
-    M3Server.TokenInterface apiToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m3_login);
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        Gson gson = gsonBuilder.create();
-        OkHttpClient clientNoToken = new OkHttpClient();
-        Retrofit retrofitNoToken = new Retrofit.Builder()
-                .baseUrl("http://app.360mooc.com")
-                .client(clientNoToken)
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-        apiToken = retrofitNoToken.create(M3Server.TokenInterface.class);
-        attemptLogin("eric.test@360learning.com",M3Server.Hash.md5("eric.test@360learning.com"));
+        M3Server.Login.login("eric.test@360learning.com", "eric.test@360learning.com");
     }
-    private void attemptLogin(final String email, final String pwd){
-        final Call<M3Server.TokenResponse> call = apiToken.postAuth(email,pwd,true);
-        call.enqueue(new Callback<M3Server.TokenResponse>() {
-            @Override
-            public void onResponse(retrofit.Response<M3Server.TokenResponse> responseCall, Retrofit retrofit) {
-                if (responseCall.body().getToken() != null) {
-                    Log.d("TEST",responseCall.body().getToken());
-                }else{
-                    Log.d("TEST","Erreur");
-                    Log.d("TEST",responseCall.body().getError().toString());
-                }
-            }
 
-            @Override
-            public void onFailure(Throwable t) {
-                Log.d("TEST","Failure");
-            }
-        });
-    }
 }
--- app/src/main/java/eric_hth/my360android/M3Server.java
+++ app/src/main/java/eric_hth/my360android/M3Server.java
@@ -1,10 +1,19 @@
 package eric_hth.my360android;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import com.squareup.okhttp.OkHttpClient;
+
 import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
 import retrofit.http.Field;
 import retrofit.http.FormUrlEncoded;
 import retrofit.http.POST;
@@ -12,7 +21,84 @@ import retrofit.http.POST;
  * Created by eric_360 on 01/03/16.
  */
 public class M3Server {
-        public enum LoggingError {
+    // MARK: public
+    public static class Login {
+        public static void login(final String email, final String pwd){
+            final Call<Login.TokenResponse> call = Login.logInInterface().postAuth(email,Login.MD5.process(pwd),true);
+            call.enqueue(new Callback<TokenResponse>() {
+                @Override
+                public void onResponse(retrofit.Response<Login.TokenResponse> responseCall, Retrofit retrofit) {
+                    if (responseCall.body().getToken() != null) {
+                        Log.d("M3Server", responseCall.body().getToken());
+                    }else{
+                        Log.d("M3Server","Erreur");
+                        Log.d("M3Server",responseCall.body().getError().toString());
+                    }
+                }
+                @Override
+                public void onFailure(Throwable t) {
+                    Log.d("M3Server","Failure");
+                }
+            });
+        }
+        private static class MD5 {
+            public static final String process(final String s) {
+                final String MD5 = "MD5";
+                try {
+                    MessageDigest digest = java.security.MessageDigest
+                            .getInstance(MD5);
+                    digest.update(s.getBytes());
+                    byte messageDigest[] = digest.digest();
+                    StringBuilder hexString = new StringBuilder();
+                    for (byte aMessageDigest : messageDigest) {
+                        String h = Integer.toHexString(0xFF & aMessageDigest);
+                        while (h.length() < 2)
+                            h = "0" + h;
+                        hexString.append(h);
+                    }
+                    return hexString.toString();
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+                return "";
+            }
+        }
+        private class TokenResponse implements Serializable {
+            @SerializedName("token")
+            @Expose
+            private String token;
+            @SerializedName("error")
+            @Expose
+            private LoggingError error;
+            public String getToken() {
+                return token;
+            }
+            public void setToken(String token) {
+                this.token = token;
+            }
+            public LoggingError getError() {
+                return error;
+            }
+            public void setError(LoggingError error) {
+                this.error = error;
+            }
+        }
+        private static TokenInterface logInInterface(){
+            Retrofit retrofitNoToken = new Retrofit.Builder()
+                    .baseUrl("http://app.360mooc.com")
+                    .client(new OkHttpClient())
+                    .addConverterFactory(GsonConverterFactory.create((new GsonBuilder()).create()))
+                    .build();
+            return retrofitNoToken.create(Login.TokenInterface.class);
+        }
+        private static interface TokenInterface {
+            @FormUrlEncoded
+            @POST("/api/tokens")
+            Call<TokenResponse> postAuth(@Field("login") String login, @Field("password") String md5Pwd, @Field("mobile") boolean mobile);
+        }
+    }
+    // MARK: public var
+    public enum LoggingError {
         @SerializedName("param_password_needed")
         PARAM_PASSWORD_NEEDED,
         @SerializedName("wrong_password")
@@ -27,52 +113,5 @@ public class M3Server {
         WRONG_IP,
         @SerializedName("wrong_signature")
         WRONG_SIGNATURE,
-    }
-    public interface TokenInterface {
-        @FormUrlEncoded
-        @POST("/api/tokens")
-        Call<TokenResponse> postAuth(@Field("login") String login, @Field("password") String md5Pwd, @Field("mobile") boolean mobile);
-    }
-    public class TokenResponse implements Serializable {
-        @SerializedName("token")
-        @Expose
-        private String token;
-        @SerializedName("error")
-        @Expose
-        private LoggingError error;
-        public String getToken() {
-            return token;
-        }
-        public void setToken(String token) {
-            this.token = token;
-        }
-        public LoggingError getError() {
-            return error;
-        }
-        public void setError(LoggingError error) {
-            this.error = error;
-        }
-    }
-    public static class Hash {
-        public static final String md5(final String s) {
-            final String MD5 = "MD5";
-            try {
-                MessageDigest digest = java.security.MessageDigest
-                        .getInstance(MD5);
-                digest.update(s.getBytes());
-                byte messageDigest[] = digest.digest();
-                StringBuilder hexString = new StringBuilder();
-                for (byte aMessageDigest : messageDigest) {
-                    String h = Integer.toHexString(0xFF & aMessageDigest);
-                    while (h.length() < 2)
-                        h = "0" + h;
-                    hexString.append(h);
-                }
-                return hexString.toString();
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            }
-            return "";
-        }
     }
 }
