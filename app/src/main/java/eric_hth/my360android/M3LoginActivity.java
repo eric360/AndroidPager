eb7c15fda92043ba38044a516c639e490adf2615
eric360 <eric-richard.hongtuanha@360learning.com>
2016-03-01 21:36:00 +0100

    Image added

--- app/src/main/java/eric_hth/my360android/M3LoginActivity.java
+++ app/src/main/java/eric_hth/my360android/M3LoginActivity.java
@@ -2,18 +2,17 @@ package eric_hth.my360android;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import retrofit.GsonConverterFactory;
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.Retrofit;
+import android.widget.EditText;
+
 public class M3LoginActivity extends AppCompatActivity {
+    EditText textView1;
+    EditText textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_m3_login);
+        setContentView(R.layout.m3login);
+        textView1 = (EditText) findViewById(R.id.textView);
+        textView2 = (EditText)findViewById(R.id.textView2);
         M3Server.Login.login("eric.test@360learning.com", "eric.test@360learning.com", new M3Server.Login.LoginCompletion() {
             @Override
             public void done(String token, M3Server.LoggingError error) {
