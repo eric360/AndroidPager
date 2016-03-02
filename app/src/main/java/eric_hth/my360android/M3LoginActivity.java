bbcde4f06a5bd8f71c4011d6ae36579a2b6f8002
eric360 <eric-richard.hongtuanha@360learning.com>
2016-03-02 17:52:04 +0100

    Code cleant

--- app/src/main/java/eric_hth/my360android/M3LoginActivity.java
+++ app/src/main/java/eric_hth/my360android/M3LoginActivity.java
@@ -1,5 +1,4 @@
 package eric_hth.my360android;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,30 +9,32 @@ import com.facebook.shimmer.ShimmerFrameLayout;
 import com.vstechlab.easyfonts.EasyFonts;
 
 public class M3LoginActivity extends AppCompatActivity {
-    EditText textView1;
-    EditText textView2;
+    private TextView title;
+    private ShimmerFrameLayout shimmer;
+    private EditText editView1;
+    private EditText editView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.m3login);
-        textView1 = (EditText) findViewById(R.id.textView);
-        textView2 = (EditText)findViewById(R.id.textView2);
-        ShimmerFrameLayout container =
-                (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
-        container.setDuration(1000);
-        container.startShimmerAnimation();
+        editView1 = (EditText) findViewById(R.id.textView);
+        editView2 = (EditText)findViewById(R.id.textView2);
+        shimmer = (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
+        shimmer.setDuration(1000);
+        shimmer.startShimmerAnimation();
         M3Server.Login.login("eric.test@360learning.com", "eric.test@360learning.com", new M3Server.Login.LoginCompletion() {
             @Override
             public void done(String token, M3Server.LoggingError error) {
                 Log.d("TEST NEW", token);
             }
+
             @Override
             public void error(Throwable t) {
                 Log.d("TEST NEW", t.toString());
             }
         });
-        TextView myTextView = (TextView)findViewById(R.id.title);
-        myTextView.setTypeface(EasyFonts.robotoThin(this));
+        title = (TextView)findViewById(R.id.title);
+        title.setTypeface(EasyFonts.robotoThin(this));
     }
 
 }
